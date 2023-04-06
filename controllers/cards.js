a60a5438c99c58c489403db503870e91a8bf714a
Максим Сычев <Dethstorm@mail.ru>
2023-04-06 21:02:26 +0300

    добавить обработку ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -44,15 +44,23 @@ module.exports.deleteCard = (req, res) => {
     .catch((err) => {
       if (err.name === 'ValidationError' || 'CastError') {
         res.status(400).send({ message: 'Невалидный идентификатор карточки' });
-      } else {
-        res.status(500).send({ message: err.message });
-      }
+      } else if (err.statusCode === 404) {
+          res.status(404).send({ message: 'Карточка не найдена' });
+        } else {
+          res.status(500).send({ message: err.message });
+        }
     })
 };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, {$addToSet: {likes: req.user._id}}, {new: true})
-    .then((likes) => res.status(200).send({data: likes}))
+    .then((likes, cardId) => {
+      if (cardId) {
+        res.status(200).send({data: likes})
+      } else {
+        res.status(404).send({ message: 'Карточка не найдена' });
+      }
+    })
     .catch((err) => {
       if (err.name === 'ValidationError' || 'CastError') {
        res.status(400).send({ message: 'Переданы некорректные данные' });
