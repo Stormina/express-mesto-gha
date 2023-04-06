29223d22048651621d6e5177bbbc8dd41fb5488c
Максим Сычев <Dethstorm@mail.ru>
2023-04-06 20:08:44 +0300

    добавить обработку ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -35,15 +35,17 @@ module.exports.createCard = (req, res) => {
 
 module.exports.deleteCard = (req, res) => {
   Card.findOneAndDelete(req.params.cardId)
-    .then((card) => {
-      if (card) {
+    .then((card) => /* {
+      if (card) { */
         res.status(200).send({data: card})
-      } else {
+      /* } else {
         res.status(404).send({ message: 'Карточка не найдена' });
-      }})
+      }} */)
     .catch((err) => {
       if (err.name === 'ValidationError' || 'CastError') {
         res.status(400).send({ message: 'Невалидный идентификатор карточки' });
+      } else if (err.statusCode === 404) {
+        res.status(404).send({ message: 'Карточка не найдена' });
       } else {
         res.status(500).send({ message: err.message });
       }
