eef9a9d3a214a3a351a892e7daec871a184dae7d
Максим Сычев <Dethstorm@mail.ru>
2023-04-15 12:50:46 +0300

    добавить авторизацию и регистрацию

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,14 +1,14 @@
 const { celebrate, Joi } = require('celebrate');
 
 module.exports.loginValidation = celebrate({
-  params: Joi.object().keys({
+  body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 });
 
 module.exports.userValidation = celebrate({
-  params: Joi.object().keys({
+  body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(/https?:\/\/(w{3}\.)?[\w\-.~:/?#[\]@!$&'\\()*+,;=]/),
@@ -24,21 +24,21 @@ module.exports.idValidation = celebrate({
 });
 
 module.exports.cardValidation = celebrate({
-  params: Joi.object().keys({
+  body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     link: Joi.string().required().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
   }),
 });
 
 module.exports.aboutValidation = celebrate({
-  params: Joi.object().keys({
+  body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 });
 
 module.exports.avatarValidation = celebrate({
-  params: Joi.object().keys({
+  body: Joi.object().keys({
     avatar: Joi.string().pattern(/https?:\/\/(w{3}\.)?[\w\-.~:/?#[\]@!$&'\\()*+,;=]/),
   }),
 });
